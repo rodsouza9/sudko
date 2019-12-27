fc1cd80d748b710964623c98c4ece035b9e86a69
Rohan D'Souza <rodsouza9@gmail.com>
2019-12-26 19:34:46 -0800

    Added mark functionality. Began button formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,9 @@ class Board extends React.Component<{}, BoardState> {
         contradicts: [38],
     };
 
+    /**
+      * // TODO: other highlight functionality
+      */
     toggleSelectSquare(i: SquareAddress) {
         this.setState( {
             ...this.state,
@@ -35,7 +38,12 @@ class Board extends React.Component<{}, BoardState> {
 
     render() {
         return(
-            <div className="board">{this.renderSquares()}</div>
+            <div className="game">
+                <div className="board">{this.renderSquares()}</div>
+                <div>
+                    <button> ro </button>
+                </div>
+            </div>
         );
     }
 
@@ -50,9 +58,10 @@ class Board extends React.Component<{}, BoardState> {
     renderSquare(i: SquareAddress) {
         const isHighlighted = this.state.highlights.includes(i);
         const isContradicting = this.state.contradicts.includes(i);
+        const marks = i % 9 + 1 == 7 ? [i%1 as SquareValue, i%5+1 as SquareValue, i%3+1 as SquareValue, i%8+1 as SquareValue] : [];
         return <Square value={i % 9 + 1 as SquareValue}
         isPermanent={false} isHighlighted = {isHighlighted} isContradicting={isContradicting}
-        markings={[]} onClick={()=>{ this.toggleSelectSquare(i)}}/>;
+        markings={marks} onClick={()=>{ this.toggleSelectSquare(i)}}/>;
     }
 }
 
@@ -68,11 +77,35 @@ interface SquareProps {
 type SquareValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 ;
 
 class Square extends React.Component<SquareProps, {}> {
+    getMarks() : boolean[] {
+        let lst : boolean[] = [];
+        for (let i = 0; i < 9; i++) {
+            lst[i] = this.props.markings.includes(i as SquareValue);
+        }
+        return lst;
+    }
     render() {
         let light : string = this.props.isHighlighted ? "highlight" : (this.props.isContradicting ? "contradict" : "");
-        return(
-            <div className={"square "+light} onClick={this.props.onClick}>{this.props.value}</div>
-        );
+        let marks : boolean[] = this.getMarks();
+        if (this.props.markings && this.props.markings.length) {
+            return(
+                <div className={"square "+light} onClick={this.props.onClick}>
+                    <div className="mark">{marks[1] ? 1 : null}</div>
+                    <div className="mark">{marks[2] ? 2 : null}</div>
+                    <div className="mark">{marks[3] ? 3 : null}</div>
+                    <div className="mark">{marks[4] ? 4 : null}</div>
+                    <div className="mark">{marks[5] ? 5 : null}</div>
+                    <div className="mark">{marks[6] ? 6 : null}</div>
+                    <div className="mark">{marks[7] ? 7 : null}</div>
+                    <div className="mark">{marks[8] ? 8 : null}</div>
+                    <div className="mark">{marks[9] ? 9 : null}</div>
+                </div>
+            );
+        } else {
+            return(
+                <div className={"square "+light} onClick={this.props.onClick}>{this.props.value}</div>
+            );
+        }
     }
 }
 
