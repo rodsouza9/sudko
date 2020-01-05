1219ab19ad0952b314c8e4bd679fbd06a2d4a7c9
Rohan D'Souza <rodsouza9@gmail.com>
2020-01-04 22:24:26 -0800

    Add format for groupings.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,48 @@ import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
 
+
+
 const App: React.FC = () => {
     const vals =
-        [null, 3, 1, 6, 7, null, 4, null, 9,
-            null, null, null, 8, 3, null, null, null, null,
-            8, 2, null, null, null, null, null, 1, null,
-            null, 7, 4, null, null, 8, 1, 6, null,
-            null, 8, null, null, 6, null, null, null, 4,
-            9, null, 2, null, null, null, null, 7, 3,
-            4, 9, null, null, 5, 7, 2, 3, null,
-            2, null, null, null, 9, null, 5, null, 7,
-            7, null, 3, 2, null, null, 6, null, 1] as Array<SquareValue | null>;
+            [null, 3    , 1    , 6    , 7    , null , 4    , null , 9    ,
+            null , null , null , 8    , 3    , null , null , null , null ,
+            8    , 2    , null , null , null , null , null , 1    , null ,
+            null , 7    , 4    , null , null , 8    , 1    , 6    , null ,
+            null , 8    , null , null , 6    , null , null , null , 4    ,
+            9    , null , 2    , null , null , null , null , 7    , 3    ,
+            4    , 9    , null , null , 5    , 7    , 2    , 3    , null ,
+            2    , null , null , null , 9    , null , 5    , null , 7    ,
+            7    , null , 3    , 2    , null , null , 6    , null , 1] as Array<SquareValue | null>;
+    const basicVals =
+            [0 ,1 ,2 ,3 ,4, 5, 6, 7, 8,
+            9 ,10 , 11 , 12    , 13    , 14 , 15 , 16 , 17 ,
+            18    ,19    , 20 , 21 , 22 , 23 , 24 , 25    , 26 ,
+            27 , 28    , 29    , 30 , 31 , 32    , 33    , 34    , 35 ,
+            36 , 37    , 38 , 39 , 40    , 41 , 42 , 43 , 44    ,
+            45    , 46 , 47    , 48 , 49 , 50 , 51 , 52    , 53    ,
+            54    , 55    , 56 , 57 , 58    , 59    , 60    , 61    , 62 ,
+            63    , 64 , 65 , 66 , 67    , 68 , 69    , 70 , 71    ,
+            72    , 73 , 74    , 75    , 76 , 77 , 78    , 79 , 80] as Array<SquareValue | null>;
+    const groups =
+        [
+            [0, 1, 2, 9, 10, 11, 18, 19, 20],
+            [3, 4, 5, 12, 13, 14, 21, 22, 23],
+            [6, 7, 8, 15, 16, 17, 24, 25, 26],
+            [27, 28, 29, 36, 37, 38, 45, 46, 47],
+            [30, 31, 32, 39, 40, 41, 48, 49, 60],
+            [33, 34, 35, 42, 43, 44, 51, 52, 53],
+            [54, 55, 56, 63, 64, 65, 72, 73, 74],
+            [57, 58, 59, 66, 67, 68, 75, 76, 77],
+            [60, 61, 62, 69, 70, 71, 78, 79, 80]
+        ];
     return (
         <div className="App">
             <header className="App-header">
-                <Board permanentValues={vals}/>
+                <Board
+                    permanentValues={vals}
+                    groupings={groups}
+                />
             </header>
         </div>
     );
@@ -39,6 +66,7 @@ interface BoardState {
 
 interface BoardProps {
     permanentValues: Array<SquareValue | null>;
+    groupings: Array<Array<SquareAddress>>;
 }
 
 class Board extends React.Component<BoardProps, BoardState> {
@@ -175,9 +203,27 @@ class Board extends React.Component<BoardProps, BoardState> {
 
     public renderSquares() {
         const list = [];
-        for (let i = 0; i < 81; i++) {
-            list.push(this.renderSquare(i));
+        for (const group of this.props.groupings) {
+            const arr = [];
+            for (const address of group) {
+                arr.push(this.renderSquare(address));
+            }
+            list.push(<div className="group">{arr}</div>);
         }
+        /*
+        const list1 = [];
+        for (const address of this.props.groupings[0]) {
+            list1.push(this.renderSquare(address));
+        }
+        const list2 = [];
+        for (const address of this.props.groupings[1]) {
+            list2.push(this.renderSquare(address));
+        }
+        /!*for (let i = 0; i < 81; i++) {
+            list.push(this.renderSquare(i));
+        }*!/
+        list.push(<div className="group">{list1}</div>);
+        list.push(<div className="group">{list2}</div>);*/
         return list;
     }
 
