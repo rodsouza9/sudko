8794abeafa27e604e1c13f334e7b8a95bee33429
Rohan D'Souza <rodsouza9@gmail.com>
2020-01-19 21:29:15 -0800

    Reformat tests with describe functions.

--- src/Validate.test.tsx
+++ src/Validate.test.tsx
@@ -2,26 +2,6 @@ import App, {SquareValue} from "./App";
 import {Contradictions, Groupings, SquareAddress, Values} from "./Types";
 import * as Validate from "./Validate";
 
-const ROWS =   [[0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8],
-    [9 , 10, 11, 12, 13, 14, 15, 16, 17],
-    [18, 19, 20, 21, 22, 23, 24, 25, 26],
-    [27, 28, 29, 30, 31, 32, 33, 34, 35],
-    [36, 37, 38, 39, 40, 41, 42, 43, 44],
-    [45, 46, 47, 48, 49, 50, 51, 52, 53],
-    [54, 55, 56, 57, 58, 59, 60, 61, 62],
-    [63, 64, 65, 66, 67, 68, 69, 70, 71],
-    [72, 73, 74, 75, 76, 77, 78, 79, 80]];
-
-const COLUMNS = [[0, 9 , 18, 27, 36, 45, 54, 63, 72],
-    [1, 10, 19, 28, 37, 46, 55, 64, 73],
-    [2, 11, 20, 29, 38, 47, 56, 65, 74],
-    [3, 12, 21, 30, 39, 48, 57, 66, 75],
-    [4, 13, 22, 31, 40, 49, 58, 67, 76],
-    [5, 14, 23, 32, 41, 50, 59, 68, 77],
-    [6, 15, 24, 33, 42, 51, 60, 69, 78],
-    [7, 16, 25, 34, 43, 52, 61, 70, 79],
-    [8, 17, 26, 35, 44, 53, 62, 71, 80]];
-
 describe("test checkGroupings", () => {
     test("with empty Values map", () => {
         const val: Values = new Map<SquareAddress, SquareValue>();
@@ -33,41 +13,55 @@ describe("test checkGroupings", () => {
         const noContradiction = new Set<SquareAddress>();
         const group: Groupings = [[0, 1, 2, 3, 4, 5, 6, 7, 8]];
 
-        test("with one item in Value", () => {
+        test("with 1 values and 0 contradictions", () => {
             const val1: Values = new Map<SquareAddress, SquareValue>();
             val1.set(0, 1);
             const set1 = Validate.checkGroupings(val1, group);
             expect(set1).toEqual(noContradiction);
         });
 
-        test("checkGroup test 2.0 - single group", () => {
+        test("with 4 values and 0 contradictions", () => {
             const val2: Values = new Map<SquareAddress, SquareValue>();
             val2.set(0, 1);
             val2.set(1, 2);
             val2.set(2, 3);
             val2.set(3, 4);
             const set2 = Validate.checkGroupings(val2, group);
-            expect(set2).toEqual(new Set<SquareAddress>());
+            expect(set2).toEqual(noContradiction);
         });
 
-        test("checkGroup test 2.0 - single group", () => {
+        test("with 4 values and 2 contradictions", () => {
             const val3: Values = new Map<SquareAddress, SquareValue>();
-            val3.set(0, 1); val3.set(1, 1); val3.set(2, 2); val3.set(3, 3);
+            val3.set(0, 1);
+            val3.set(1, 1);
+            val3.set(2, 2);
+            val3.set(3, 3);
             const set3 = Validate.checkGroupings(val3, group);
             const set3E = new Set<SquareAddress>();
-            set3E.add(0); set3E.add(1);
+            set3E.add(0);
+            set3E.add(1);
             expect(set3).toEqual(set3E);
+        });
+
+        test("with 4 values and 4 contradictions", () => {
             const val4: Values = new Map<SquareAddress, SquareValue>();
-            val4.set(0, 1); val4.set(1, 1); val4.set(2, 2); val4.set(3, 2);
+            val4.set(0, 1);
+            val4.set(1, 1);
+            val4.set(2, 2);
+            val4.set(3, 2);
             const set4 = Validate.checkGroupings(val4, group);
             const set4E = new Set<SquareAddress>();
-            set4E.add(0); set4E.add(1); set4E.add(2); set4E.add(3);
+            set4E.add(0);
+            set4E.add(1);
+            set4E.add(2);
+            set4E.add(3);
             expect(set4).toEqual(set4E);
         });
     });
 });
 
-// not entirely random but somewhat random
+/*
+// Not deterministic but maintained for future reference.
 function addToVal(toContradict: boolean, values: Values, groupings: Groupings): Contradictions {
     const randomGroup = groupings[Math.floor(Math.random() * groupings.length)];
     const randomElement = randomGroup[Math.floor(Math.random() * randomGroup.length)];
@@ -152,3 +146,4 @@ test("checkGroup test 3 - random", () => {
         randomTestCheckGroup(num, group);
     }
 });
+*/
