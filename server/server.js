b502650babc54e9af0e3a54c600a06674e42ceff
Rohan D'Souza <rodsouza9@gmail.com>
2020-05-28 21:57:18 -0700

    Add DateButton component. Add date path request in server.js functionality. Connect front and and back end.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html')));
-const datetime = new Date();
-app.get('/date', (req, res) => res.send(datetime.toISOString()))
-app.listen(9000);
-console.log("This app is running on http://localhost:9000/")
+app.get('/date', (req, res) => res.send(new Date().toUTCString()))
+app.listen(5000);
+console.log("This app is running on http://localhost:5000/")
