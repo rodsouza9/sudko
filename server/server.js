e6d9fe128fdc63b7be1e256a1764fca515868cbe
Rohan D'Souza <rodsouza9@gmail.com>
2020-05-28 18:44:19 -0700

    Add date request.

--- server/server.js
+++ server/server.js
@@ -3,5 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html')));
+const datetime = new Date();
+app.get('/date', (req, res) => res.send(datetime.toISOString()))
 app.listen(9000);
 console.log("This app is running on http://localhost:9000/")
