aac35a4faef96d52e7cb4681290596e948ce0e82
Rohan D'Souza <rodsouza9@gmail.com>
2020-06-15 12:25:37 -0700

    Get authentication routes to work w passport. Use FileStore instead of database.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,67 @@
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require('uuid');
 const session = require('express-session')
+const FileStore = require('session-file-store')(session); // need to be replaced w database
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
+
+// this is
+const users = [
+    {id: '2f24vvg', email: 'test@test.com', password: 'password'}
+]
+// Passport setup
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    (email, password, done) => {
+        console.log('Inside local strategy callback')
+        // here is where you make a call to the database
+        // to find the user based on their username or email address
+        // for now, we'll just pretend we found that it was users[0]
+        const user = users[0]
+        // change this to database stuff later
+        if(email === user.email && password === user.password) {
+            console.log('Local strategy returned true')
+            return done(null, user)
+        }
+    }
+));
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+    console.log('Inside serializeUser callback. User id is save to the session file store here')
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log('Inside deserializeUser callback')
+    console.log(`The user id passport saved in the session file store is: ${id}`)
+    const user = users[0].id === id ? users[0] : false;
+    done(null, user);
+});
+
+
+// express server
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 // add & configure middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(session({
     genid: (req) => {
-        console.log('Inside the session middleware')
-        console.log(req.sessionID)
+        console.log('Inside the session middleware genid function')
+        console.log(`Request object sessionID from client: ${req.sessionID}`)
         return uuidv4() // use UUIDs for session IDs
     },
+    store: new FileStore(),
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
@@ -33,11 +76,31 @@ app.get('/login', (req, res) => {
     res.send(`You got the login page!\n`)
 })
 
-app.post('/login', (req, res) => {
-    console.log('Inside POST /login callback function');
-    console.log(req.body);
-    res.send(`You posted to the login page!\n`);
+app.post('/login', (req, res, next) => {
+    console.log('Inside POST /login callback')
+    passport.authenticate('local', (err, user, info) => {
+        console.log('Inside passport.authenticate() callback');
+        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+        console.log(`req.user: ${JSON.stringify(req.user)}`)
+        req.login(user, (err) => {
+            console.log('Inside req.login() callback')
+            console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+            console.log(`req.user: ${JSON.stringify(req.user)}`)
+            return res.send('You were authenticated & logged in!\n');
+        })
+    })(req, res, next);
 })
 
+app.get('/authrequired', (req, res) => {
+    console.log('Inside GET /authrequired callback')
+    console.log(`User authenticated? ${req.isAuthenticated()}`)
+    if(req.isAuthenticated()) {
+        res.send('you hit the authentication endpoint\n')
+    } else {
+        res.redirect('/')
+    }
+})
+
+//listen bruh
 app.listen(5000);
 console.log("This app is running on http://localhost:5000/")
