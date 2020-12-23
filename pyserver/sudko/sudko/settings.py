1c2bbd911a03779fa37fa1ef994a6103e9b0bfe2
Rohan D'Souza <rodsouza9@gmail.com>
2020-12-23 15:26:04 -0800

    Add basic Django functionality.

--- pyserver/sudko/sudko/settings.py
+++ pyserver/sudko/sudko/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sudko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rohan',
+        'HOST': 'localhost',
+        'PORT': '54320',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
     }
 }
 
