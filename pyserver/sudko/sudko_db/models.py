7a275d9327958de6e458557117ba2d8b34ffd091
Rohan D'Souza <rodsouza9@gmail.com>
2020-12-25 19:24:57 -0800

    Made migration for Puzzle.

--- pyserver/sudko/sudko_db/models.py
+++ pyserver/sudko/sudko_db/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib import admin
+from django.core.validators import validate_comma_separated_integer_list
 
-# Create your models here.
+LENGTH_OF_PUZZLE = 250
+
+
+"""
+Puzzle: used to store initial state of sudko games
+- name : name of puzzle
+- values: array 
+- description: description of puzzle
+- likes: the number of likes
+- difficulty: {eazy, medium, hard, N/A}
+- num_attempts : number of users to have started the puzzle
+- num_solved : number of users to have completed the puzzle
+- average_time : average completion time of puzzle
+- best_time : best time of puzzle
+
+"""
+
+class Puzzle(models.Model):
+    name = models.CharField(max_length=200)
+    values = models.CharField(max_length=LENGTH_OF_PUZZLE)
+    description = models.TextField(blank=True)
+
+    EASY, MED, HARD, NA = "E", "M", "H", "N"
+    DIFF_CHOICES = [
+        (EASY, "easy"),
+        (MED, "medium"),
+        (HARD, "hard"),
+        (NA, "N/A")
+    ]
+    difficulty = models.CharField(
+        max_length=1,
+        choices=DIFF_CHOICES,
+        default=NA,
+    )
+
+    num_attempts = models.PositiveBigIntegerField()
+    num_solved = models.PositiveBigIntegerField()
+    average_time = models.DurationField()
+    best_time = models.DurationField()
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PuzzleAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Puzzle, PuzzleAdmin)
+
